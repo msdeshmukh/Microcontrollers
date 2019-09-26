ed5ef838a1e46570b619af928954631691d6f1e3
Ryan Myers <ryanmyers@Ryans-MacBook-Pro-2.local>
2019-09-26 16:41:27 -0700

    some work done on first function

--- Assignment1/delay.c
+++ Assignment1/delay.c
@@ -6,9 +6,14 @@
  */
 
 #include "delay.h"
+#include "msp.h"
+#include <stdint.h>
 
-void set_DCO(int MHz_freq) {
-
+void set_DCO(uint32_t MHz_freq) {
+    CS->KEY = CS_KEY_VAL;
+    CS->CTL0 &= ~CS_CTL0_DCOTUNE_MASK;
+    CS->CTL0 |= CS_CTL0_DCORSEL_MASK & MHz_freq;
+    CS->KEY = 0;
 }
 
 void delay_us(int us_delay) {
--- Assignment1/delay.h
+++ Assignment1/delay.h
@@ -8,7 +8,14 @@
 #ifndef DELAY_H_
 #define DELAY_H_
 
-void set_DCO(int MHz_freq);
+#define FREQ_1POINT5_MHz 0x0
+#define FREQ_3_MHz 0x1
+#define FREQ_6_MHz 0x2
+#define FREQ_12_MHz 0x3
+#define FREQ_24_MHz 0x4
+#define FREQ_48_MHz 0x5
+
+void set_DCO(uint32_t MHz_freq);
 void delay_us(int us_delay);
 
 #endif /* DELAY_H_ */
