d5c5a175a32e7a1c89b106af8edef513da096467
Ryan Myers <ryanmyers@Ryans-MacBook-Pro-2.local>
2019-09-26 15:56:12 -0700

    added assignment 1 with function prototypes

---
+++ Assignment1/delay.c
@@ -0,0 +1,18 @@
+/*
+ * delay.c
+ *
+ *  Created on: Sep 26, 2019
+ *      Author: ryanmyers
+ */
+
+#include "delay.h"
+
+void set_DCO(int MHz_freq) {
+
+}
+
+void delay_us(int us_delay) {
+
+}
+
+
---
+++ Assignment1/delay.h
@@ -0,0 +1,14 @@
+/*
+ * delay.h
+ *
+ *  Created on: Sep 26, 2019
+ *      Author: ryanmyers
+ */
+
+#ifndef DELAY_H_
+#define DELAY_H_
+
+void set_DCO(int MHz_freq);
+void delay_us(int us_delay);
+
+#endif /* DELAY_H_ */
