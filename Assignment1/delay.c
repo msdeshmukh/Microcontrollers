ed6fe0586c22db86260d366dea39648effbc34eb
Ryan Myers <ryanmyers@Ryans-MacBook-Pro-2.local>
2019-09-26 19:58:12 -0700

    delay_us() written

--- Assignment1/delay.c
+++ Assignment1/delay.c
@@ -17,7 +17,32 @@ void set_DCO(uint32_t MHz_freq) {
 }
 
 void delay_us(int us_delay) {
-
+    uint32_t dco_freq;
+    uint32_t dco_rsel = (CS->CTL0 & CS_CTL0_DCORSEL_MASK) >> CS_CTL0_DCORSEL_OFS;
+    int dco_tune = (CS->CTL0 & CS_CTL0_DCOTUNE_MASK) >> CS_CTL0_DCOTUNE_OFS;
+    if (dco_tune & DCOTUNE_SIGN_MASK) {
+        dco_tune = -1 * (~dco_tune + 1);
+    }
+    switch (dco_rsel) {
+    case DCO_1POINT5_MHz:
+        dco_freq = FREQ_1POINT5_MHz + dco_tune;
+        break;
+    case DCO_3_MHz:
+        dco_freq = FREQ_3_MHz + dco_tune;
+        break;
+    case DCO_6_MHz:
+        dco_freq = FREQ_6_MHz + dco_tune;
+        break;
+    case DCO_12_MHz:
+        dco_freq = FREQ_12_MHz + dco_tune;
+        break;
+    case DCO_24_MHz:
+        dco_freq = FREQ_24_MHz + dco_tune;
+        break;
+    case DCO_48_MHz:
+        dco_freq = FREQ_48_MHz + dco_tune;
+        break;
+    }
 }
 
 
--- Assignment1/delay.h
+++ Assignment1/delay.h
@@ -8,12 +8,19 @@
 #ifndef DELAY_H_
 #define DELAY_H_
 
-#define FREQ_1POINT5_MHz 0x0
-#define FREQ_3_MHz 0x1
-#define FREQ_6_MHz 0x2
-#define FREQ_12_MHz 0x3
-#define FREQ_24_MHz 0x4
-#define FREQ_48_MHz 0x5
+#define DCO_1POINT5_MHz 0x0
+#define DCO_3_MHz 0x1
+#define DCO_6_MHz 0x2
+#define DCO_12_MHz 0x3
+#define DCO_24_MHz 0x4
+#define DCO_48_MHz 0x5
+#define FREQ_1POINT5_MHz 1500000u
+#define FREQ_3_MHz 3000000u
+#define FREQ_6_MHz 6000000u
+#define FREQ_12_MHz 12000000u
+#define FREQ_24_MHz 24000000u
+#define FREQ_48_MHz 48000000u
+#define DCOTUNE_SIGN_MASK 0x200
 
 void set_DCO(uint32_t MHz_freq);
 void delay_us(int us_delay);
