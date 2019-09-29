e77fcc97a169d527d4bee6e0443065bffff0b94f
Ryan Myers <ryanmyers@Ryans-MacBook-Pro-2.local>
2019-09-28 21:33:15 -0700

    Init and command functions partially written

--- Assignment2/lcd_driver.c
+++ Assignment2/lcd_driver.c
@@ -22,10 +22,63 @@ Write_char_LCD(char to_write, uint32_t line, uint32_t col) {
 
 }
 
+Write_command_LCD(uint8_t command) {
+    LCD_CTL &= ~LCD_DATA_MASK;
+    LCD_CTL |= command & LCD_DATA_MASK;
+    LCD_CTL &= ~LCD_RW_BIT;
+
+    LCD_CTL |= LCD_EN_BIT;
+    us_delay(1);
+    LCD_CTL &= ~LCD_EN_BIT;
+
+    command = command << 4;
+    LCD_CTL &= ~LCD_DATA_MASK;
+    LCD_CTL |= command & LCD_DATA_MASK;
+
+    LCD_CTL |= LCD_EN_BIT;
+    us_delay(1);
+    LCD_CTL &= ~LCD_EN_BIT;
+}
+
 Initialize_LCD() {
-    P2->DIR |= LCD_PIN_MASK;    // Set P2.0-6 to output
-    P2->REN &= ~LCD_PIN_MASK;   // Disable P2.0-6 pull up/down resistor
-    P2->SEL0 &= ~LCD_PIN_MASK;  // Setup P2.0-6 as GPIO
-    P2->SEL1 &= ~LCD_PIN_MASK;
+    P4->DIR |= LCD_PIN_MASK;    // Set P2.0-6 to output
+    P4->REN &= ~LCD_PIN_MASK;   // Disable P2.0-6 pull up/down resistor
+    P4->SEL0 &= ~LCD_PIN_MASK;  // Setup P2.0-6 as GPIO
+    P4->SEL1 &= ~LCD_PIN_MASK;
+    LCD_CTL = 0;
+
+    us_delay(40000);
+    LCD_CTL |= LCD_DATA_MASK & 0x30;
+    us_delay(5000);
+
+    LCD_CTL |= LCD_EN_BIT;
+    us_delay(1);
+    LCD_CTL &= ~LCD_EN_BIT;
+
+    us_delay(160);
+
+    LCD_CTL |= LCD_EN_BIT;
+    us_delay(1);
+    LCD_CTL &= ~LCD_EN_BIT;
+
+    us_delay(160);
+
+    LCD_CTL |= LCD_EN_BIT;
+    us_delay(1);
+    LCD_CTL &= ~LCD_EN_BIT;
+
+    us_delay(160);
+
+    LCD_CTL &= ~LCD_DATA_MASK;
+    LCD_CTL |= LCD_DATA_MASK & 0x20;
+
+    LCD_CTL |= LCD_EN_BIT;
+    us_delay(1);
+    LCD_CTL &= ~LCD_EN_BIT;
+
+    Write_command_LCD(FUNCTION_SET);
+    Write_command_LCD(SET_CURSOR);
+    Write_command_LCD(DISPLAY_ON);
+    Write_command_LCD(SET_ENTRY_MODE);
 
 }
--- Assignment2/lcd_driver.h
+++ Assignment2/lcd_driver.h
@@ -10,16 +10,23 @@
 
 #include <stdint.h>
 
-#define LCD_DATA P2->OUT
-#define LCD_PIN_MASK 0x7F
-#define LCD_DATA_MASK 0x0F
+#define LCD_CTL P4->OUT
+#define LCD_PIN_MASK 0xFE
+#define LCD_DATA_MASK 0xF0
 #define LCD_EN_BIT 0x40
 #define LCD_RS_BIT 0x20
 #define LCD_RW_BIT 0x10
 
+#define WAKE_UP 0x30
+#define FUNCTION_SET 0x23
+#define SET_CURSOR 0x10
+#define DISPLAY_ON 0x0F
+#define SET_ENTRY_MODE 0x06
+
 Clear_LCD(); // clear the display
 Home_LCD(); // move the cursor to the top left of the LCD
 Write_char_LCD(char to_write, uint32_t line, uint32_t col); // write a character to the LCD
+Write_command_LCD(uint8_t command);
 Initialize_LCD(); // run initialization commands on LCD
 
 
