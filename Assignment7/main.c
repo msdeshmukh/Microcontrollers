2911b1f5e9f8526698dc72a09335044c1949e3a7
msdeshmukh <mdeshmuk@calpoly.edu>
2019-10-17 19:22:04 -0700

    latest commit

--- Assignment7/main.c
+++ Assignment7/main.c
@@ -8,7 +8,11 @@ void main(void)
 {
 	WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;		// stop watchdog timer
 	Initialize_SPI();
+	__enable_irq();
 	Send_DAC_Voltage(2.0);
 
-	while(1);
+	while(1)
+	{
+	    Send_DAC_Voltage(2.0);
+	}
 }
