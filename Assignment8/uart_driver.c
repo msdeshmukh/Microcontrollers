365e981fdb4ef00798eaf26c323fe8772f39c0e6
Ryan Myers <ryanmyers@Ryans-MacBook-Pro-2.local>
2019-10-24 20:30:55 -0700

    Initial assignment 8 push

---
+++ Assignment8/uart_driver.c
@@ -0,0 +1,9 @@
+/*
+ * uart_driver.c
+ *
+ *  Created on: Oct 24, 2019
+ *      Author: ryanmyers
+ */
+
+#include "msp.h"
+#include "uart_driver.h"
---
+++ Assignment8/uart_driver.h
@@ -0,0 +1,13 @@
+/*
+ * uart_driver.h
+ *
+ *  Created on: Oct 24, 2019
+ *      Author: ryanmyers
+ */
+
+#ifndef UART_DRIVER_H_
+#define UART_DRIVER_H_
+
+
+
+#endif /* UART_DRIVER_H_ */
